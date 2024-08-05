3335676c9e605f7c152aadca2d091cb2b41e4729
Mosi <mosisu07@hismax.onmicrosoft.com>
2024-08-05 15:18:10 +0800

    layout: added themeProvider & theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./constants/theme";
 function App() {
   const [age, setAge] = useState("");
 
@@ -16,23 +18,25 @@ function App() {
       <div style={{ textAlign: "center" }}>
         <h1>Hello, World!</h1>
       </div>
-
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box>
+          <FormControl sx={{ m: 1, minWidth: 120, bgcolor: "base.black" }}>
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+              sx={{ color: "base.white" }}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </ThemeProvider>
     </>
   );
 }
