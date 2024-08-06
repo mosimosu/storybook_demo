180fa2601700cf47dd986e9feccc31d4bb93c1b3
Mosi <mosisu07@hismax.onmicrosoft.com>
2024-08-06 17:44:47 +0800

    mod: importing way of StyledButton

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import theme from "./constants/theme";
 import CustomButton from "./stories/CustomButton";
-import StyledButton from "./stories/StyledButton";
+import { StyledButton } from "./stories/StyledButton";
 
 function App() {
   const [age, setAge] = useState("");
