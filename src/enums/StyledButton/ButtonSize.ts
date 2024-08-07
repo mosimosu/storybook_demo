3c91287bddd6d32503461805c28a354f1083dac5
Mosi <mosisu07@hismax.onmicrosoft.com>
2024-08-07 14:47:19 +0800

    chore: created ButtonSize enum

---
+++ src/enums/StyledButton/ButtonSize.ts
@@ -0,0 +1,13 @@
+/**
+ * 按鈕大小列舉
+ * @enum {string}
+ * @example
+ * import { SizeType } from "enums/StyledButton/SizeType";
+ * console.log(SizeType.small);
+ */
+export enum ButtonSize {
+  small = "small",
+  medium = "medium",
+  large = "large",
+  none = "",
+}
