a29059cc7c55d3e65b0f6d7929ed55469cacb23e
Mosi <mosisu07@hismax.onmicrosoft.com>
2024-08-07 16:34:44 +0800

    chore: created ButtonType Enum

---
+++ src/enums/StyledButton/ButtonType.ts
@@ -0,0 +1,8 @@
+export enum ButtonType {
+  primary = "primary",
+  secondary = "secondary",
+  warning = "warning",
+  alarm = "alarm",
+  outlinePrimary = "outline-primary",
+  outlineSecondary = "outline-secondary",
+}
