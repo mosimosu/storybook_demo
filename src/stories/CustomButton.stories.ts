0d1453f376126caf7edcb974929a77bff42f06ad
Mosi <mosisu07@hismax.onmicrosoft.com>
2024-08-07 10:06:02 +0800

    chore: modified component path

--- src/stories/CustomButton.stories.ts
+++ src/stories/CustomButton.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import CustomButton from "./CustomButton";
+import CustomButton from "../components/CustomButton";
 
 const meta = {
   title: "Example/CustomButton",
