616871384d3f0758a1c3698b6039c42a41fabf9a
Mosi <mosisu07@hismax.onmicrosoft.com>
2024-08-07 16:35:17 +0800

    refactor: replaced array with enum

--- src/stories/StyledButton.stories.ts
+++ src/stories/StyledButton.stories.ts
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import StyledButton from "../components/StyledButton";
 import { ButtonSize } from "../enums/StyledButton/ButtonSize";
+import { ButtonType } from "../enums/StyledButton/ButtonType";
 
 const meta: Meta<typeof StyledButton> = {
   title: "Example/StyledButton",
@@ -13,25 +14,18 @@ const meta: Meta<typeof StyledButton> = {
   argTypes: {
     $mode: {
       control: "select",
-      options: [
-        "primary",
-        "secondary",
-        "warning",
-        "alarm",
-        "outline-primary",
-        "outline-secondary",
-      ],
+      options: Object.values(ButtonType),
     },
     $size: {
       control: "radio",
-      options: ["small", "medium", "large"],
+      options: Object.values(ButtonSize),
     },
     $isSelected: { control: "boolean" },
     disabled: { control: "boolean" },
     onClick: { action: "clicked" },
   },
   args: {
-    $mode: "primary",
+    $mode: ButtonType.primary,
     disabled: false,
     $isSelected: false,
     $size: ButtonSize.none,
@@ -45,42 +39,42 @@ type Story = StoryObj<typeof StyledButton>;
 
 export const Primary: Story = {
   args: {
-    $mode: "primary",
+    $mode: ButtonType.primary,
     disabled: false,
   },
 };
 
 export const Secondary: Story = {
   args: {
-    $mode: "secondary",
+    $mode: ButtonType.secondary,
     disabled: false,
   },
 };
 
 export const Warning: Story = {
   args: {
-    $mode: "warning",
+    $mode: ButtonType.warning,
     disabled: false,
   },
 };
 
 export const Alarm: Story = {
   args: {
-    $mode: "alarm",
+    $mode: ButtonType.alarm,
     disabled: false,
   },
 };
 
 export const OutlinePrimary: Story = {
   args: {
-    $mode: "outline-primary",
+    $mode: ButtonType.outlinePrimary,
     disabled: false,
   },
 };
 
 export const OutlineSecondary: Story = {
   args: {
-    $mode: "outline-secondary",
+    $mode: ButtonType.outlineSecondary,
     disabled: false,
   },
 };
