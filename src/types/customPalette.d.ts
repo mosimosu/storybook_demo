29f7f9f24b71dac02147c989655c6bd5473d21dd
Mosi <mosisu07@gmail.com>
2024-08-07 17:08:20 +0800

    chore: commented useless code.

--- src/types/customPalette.d.ts
+++ src/types/customPalette.d.ts
@@ -1,4 +1,4 @@
-import { PaletteOptions } from "@mui/material/styles/createPalette";
+// import { PaletteOptions } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
   interface PaletteOptions {
