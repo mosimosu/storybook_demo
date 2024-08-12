5501b4da30096e00948f3a5a8625378366d20b74
Mosi <mosisu07@gmail.com>
2024-08-12 08:57:18 +0800

    Create enum ButtonText & ButtonIcons

---
+++ src/enums/IconButton/IconButtonIcon.ts
@@ -0,0 +1,20 @@
+/**
+ *按鈕圖示列舉
+ * @enum {string}
+ * @example
+ * import {IconButtonIcon} from './enums/IconButton/ButtonIcon'
+ */
+export enum IconButtonIcon {
+  read = "./assets/images/icons/read.svg",
+  signature = "../../assets/images/icons/signature.svg",
+  version = "../../assets/images/icons/version.svg",
+  delete = "../../assets/images/icons/delete.svg",
+  details = "../../assets/images/icons/details.svg",
+  copy = "../../assets/images/icons/copy.svg",
+  reply = "../../assets/images/icons/reply.svg",
+  view = "../../assets/images/icons/view.svg",
+  edit = "../../assets/images/icons/edit.svg",
+  save = "../../assets/images/icons/save.svg",
+  cancel = "../../assets/images/icons/cancel.svg",
+  quickEdit = "../../assets/images/icons/quickEdit.svg",
+}
---
+++ src/enums/IconButton/IconButtonText.ts
@@ -0,0 +1,21 @@
+/**
+ * 按鈕文字列舉
+ * @enum {string}
+ * @example
+ * import {IconButtonText} from './enums/IconButton/IconButtonText'
+ * console.log(ButtonText.read) // '閱讀'
+ */
+export enum IconButtonText {
+  read = "閱讀",
+  signature = "簽章資訊",
+  version = "版次",
+  delete = "刪除",
+  details = "明細",
+  copy = "複製",
+  reply = "回覆",
+  view = "查看",
+  edit = "編輯",
+  save = "儲存",
+  cancel = "取消",
+  quickEdit = "快速修改",
+}
