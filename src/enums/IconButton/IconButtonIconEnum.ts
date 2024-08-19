bb3a6a6115cab47757093d41482e9be0c7d36300
Mosi <mosisu07@gmail.com>
2024-08-19 16:18:03 +0800

    chore: created icon enum

---
+++ src/enums/IconButton/IconButtonIconEnum.ts
@@ -0,0 +1,22 @@
+import CopySVG from "../../assets/images/icons/copy.svg?react";
+import ReplySVG from "../../assets/images/icons/reply.svg?react";
+import ViewSVG from "../../assets/images/icons/view.svg?react";
+import EditSVG from "../../assets/images/icons/edit_square.svg?react";
+import SaveSVG from "../../assets/images/icons/save.svg?react";
+import CancelSVG from "../../assets/images/icons/cancel.svg?react";
+import QuickEditSVG from "../../assets/images/icons/quickEdit.svg?react";
+
+/**
+ *按鈕圖示列舉
+ * @example
+ * import {IconButtonIcon} from './enums/IconButton/ButtonIcon'
+ */
+export const IconButtonIconEnum = {
+  copy: CopySVG,
+  reply: ReplySVG,
+  view: ViewSVG,
+  edit: EditSVG,
+  save: SaveSVG,
+  cancel: CancelSVG,
+  quickEdit: QuickEditSVG,
+};
