ad75929a553224bf17f7cfaac2443106ef54416e
Mosi <mosisu07@gmail.com>
2024-08-20 11:11:24 +0800

    chore: created Typography enum

---
+++ src/enums/Theme/typographyEnum.ts
@@ -0,0 +1,13 @@
+export enum Typography {
+  FontFamily = "Noto Sans TC",
+  TextTinyFontSize = "0.75rem",
+  TextTinyLineHeight = "1rem",
+  TextSmallFontSize = "0.875rem",
+  TextSmallLineHeight = "1.25rem",
+  TextMediumFontSize = "1rem",
+  TextMediumLineHeight = "1.5rem",
+  TextLargeFontSize = "1.125rem",
+  TextLargeLineHeight = "1.75rem",
+  TextXLargeFontSize = "1.25rem",
+  TextXLargeLineHeight = "1.75rem",
+}
