1880ff0e7189e1d9b593a0ac283af8a33edabca9
Mosi <mosisu07@gmail.com>
2024-08-20 11:26:15 +0800

    refactor: iconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,32 +1,65 @@
 import { Button } from "@mui/material";
-import styled from "styled-components";
+import styled, { ThemeProvider, css } from "styled-components";
+import theme from "../../theme";
 import { IconButtonTextEnum } from "../../enums/IconButton/IconButtonTextEnum";
-import { IconButtonIcon } from "./IconButtonIcon";
-export const StyledIconButton = styled(Button)`
+import { IconButtonIconEnum } from "../../enums/IconButton/IconButtonIconEnum";
+
+// 定義 IconButton 的 props
+interface IconButtonProps {
+  // 按鈕名稱
+  name?: string;
+  // 按鈕圖示
+  icon: keyof typeof IconButtonIconEnum;
+  // 是否禁用
+  disabled: boolean;
+  // Table 類型
+  tableType: string;
+  // 其他屬性
+  props?: [key: string];
+  // 點擊功能
+  onClick?: () => void;
+}
+
+interface TextProps {
+  tableType: string;
+}
+const textStyle = css<TextProps>`
+  ${(props) => props.theme.typography?.textMedium}
+  color: ${(props) => {
+    switch (props.tableType) {
+      case "a":
+        return props.theme.palette.blue[400];
+      case "b":
+        return props.theme.palette.base.black;
+      default:
+        return props.theme.palette.blue[400];
+    }
+  }}
+`;
+
+// 定義 IconButton 的樣式
+export const StyledIconButton = styled(Button)<TextProps>`
+  ${textStyle};
   display: flex;
   align-items: center;
   gap: 4px;
 `;
-interface IconButtonProps {
-  tag: string;
-  name: React.SVGProps<SVGSVGElement>;
-  disabled: boolean;
-  props: [key: string];
-}
 
 // 定義一個 Wrapper 來封裝 IconButton 匯出到 storybook
-const StyledIconButtonWrapper = ({
-  tag = IconButtonTextEnum.read,
-  name,
+const IconButton = ({
+  name = IconButtonTextEnum.read,
+  icon = "read",
+  tableType = "a",
   disabled = false,
-  ...props
-}: IconButtonProps): React.ReactNode => {
+}: IconButtonProps): JSX.Element => {
+  const IconComponent = IconButtonIconEnum[icon];
   return (
-    <StyledIconButton {...props} disabled={disabled}>
-      {IconButtonIcon.read(name)}
-      {tag}
-    </StyledIconButton>
+    <ThemeProvider theme={theme}>
+      <StyledIconButton disabled={disabled} tableType={tableType}>
+        <IconComponent title={name} />
+        {name}
+      </StyledIconButton>
+    </ThemeProvider>
   );
 };
-
-export default StyledIconButtonWrapper;
+export default IconButton;
