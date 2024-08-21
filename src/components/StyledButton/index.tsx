64597f68768717b5ae7e2ef7127206dc82349fbe
Mosi <mosisu07@gmail.com>
2024-08-21 09:08:23 +0800

    refactor: enum importing way

--- src/components/StyledButton/index.tsx
+++ src/components/StyledButton/index.tsx
@@ -1,18 +1,18 @@
 import { Button as MuiButton } from "@mui/material";
 import styled, { css } from "styled-components";
-import { ButtonSize } from "../../enums/Button/ButtonSizeEnum";
-import { ButtonType } from "../../enums/Button/ButtonTypeEnum";
+import { ButtonSizeEnum } from "../../enums/Button/ButtonSizeEnum";
+import { ButtonTypeEnum } from "../../enums/Button/ButtonTypeEnum";
 
 // Button 的 interface
 interface StyledButtonProps {
   // 按鈕名稱
   name: string;
   // 按鈕樣式
-  $mode: ButtonType;
+  $mode: ButtonTypeEnum;
   // 是否禁用
   disabled: boolean;
   // 大小
-  $size: ButtonSize;
+  $size: keyof typeof ButtonSizeEnum;
   // 是否選取
   $isSelected: boolean;
   // 點擊功能
@@ -22,16 +22,16 @@ interface StyledButtonProps {
 // 共同設定 props
 interface CommonSettingProps {
   // 按鈕樣式
-  $mode?: ButtonType;
+  $mode?: keyof typeof ButtonTypeEnum;
   // 大小
-  $size?: ButtonSize;
+  $size?: keyof typeof ButtonSizeEnum;
 }
 
 // 共通設定
 const commonSetting = css<CommonSettingProps>`
   ${(props) => {
     switch (props.$size) {
-      case ButtonSize.small:
+      case "Small":
         return "height: 32px;";
       default:
         return "height: 40px;";
@@ -44,7 +44,7 @@ const commonSetting = css<CommonSettingProps>`
   border-radius: 6px;
   ${(props) => {
     switch (props.$size) {
-      case ButtonSize.small:
+      case "Small":
         return "padding: 4px 8px;";
       default:
         return "padding: 0rem 1rem;";
@@ -199,17 +199,17 @@ const outlineSecondaryStyle = css`
 // 根據傳入的 props 設定 Button 的樣式
 const getButtonStyle = (props: StyledButtonProps) => {
   switch (props.$mode) {
-    case ButtonType.primary:
+    case ButtonTypeEnum.Primary:
       return primaryStyle;
-    case ButtonType.secondary:
+    case ButtonTypeEnum.Secondary:
       return secondaryStyle;
-    case ButtonType.warning:
+    case ButtonTypeEnum.Warning:
       return warningStyle;
-    case ButtonType.alarm:
+    case ButtonTypeEnum.Alarm:
       return alarmStyle;
-    case ButtonType.outlinePrimary:
+    case ButtonTypeEnum.OutlinePrimary:
       return outlinePrimaryStyle;
-    case ButtonType.outlineSecondary:
+    case ButtonTypeEnum.OutlineSecondary:
       return outlineSecondaryStyle;
     default:
       return primaryStyle;
@@ -225,15 +225,15 @@ export const Button = styled(MuiButton)<StyledButtonProps>`
 
 // 定義一個 Wrapper 來包裹 Button 匯出
 const StyledButtonWrapper = ({
-  $mode = ButtonType.primary, // 預設 $mode 來呈現 primary 按鈕
-  $size = ButtonSize.none, // 預設 $size 來呈現 medium 按鈕
+  $mode = ButtonTypeEnum.Primary, // 預設 $mode 來呈現 primary 按鈕
+  $size = "Medium", // 預設 $size 來呈現 medium 按鈕
   $isSelected = false, // 預設 $isSelected 來呈現未被選中
   name = "按鈕名稱", // 預設 $name 來呈現按鈕名稱
   ...props // 其他 props
 }: StyledButtonProps) => {
   // 是否被選中
   return (
-    <StyledButton
+    <Button
       name={name}
       $mode={$mode}
       $size={$size}
@@ -241,7 +241,7 @@ const StyledButtonWrapper = ({
       {...props}
     >
       {name}
-    </StyledButton>
+    </Button>
   );
 };
 
