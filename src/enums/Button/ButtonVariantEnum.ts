922691961ca87075eea1eb6a1ba174549e12cd68
Mosi <mosisu07@gmail.com>
2024-08-21 16:09:44 +0800

    chore: created variant Enum

---
+++ src/enums/Button/ButtonVariantEnum.ts
@@ -0,0 +1,12 @@
+/**
+ * 按鈕樣式列舉
+ * @enum {string}
+ * @example
+ * import { ButtonVariantEnum } from "enums/Button/ButtonVariantEnum";
+ * console.log(ButtonVariantEnum.Contained);
+ */
+export enum ButtonVariantEnum {
+  Contained = "contained",
+  Outlined = "outlined",
+  Text = "text",
+}
