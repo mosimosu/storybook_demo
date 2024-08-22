f389fc4f76e995ee4268c37b22c3a77d6ff8bba4
Mosi <mosisu07@gmail.com>
2024-08-22 18:18:36 +0800

    chore: added iconButton enum

---
+++ src/enums/IconButton/IconButtonColorEnum.ts
@@ -0,0 +1,11 @@
+/**
+ * 按鈕顏色列舉
+ * @enum {string}
+ * @example
+ * import { ButtonTypeEnum } from "enums/Button/ButtonTypeEnum";
+ * console.log(ButtonTypeEnum.Primary);
+ */
+export enum IconButtonColorEnum {
+  Primary = "primary",
+  Success = "success",
+}
