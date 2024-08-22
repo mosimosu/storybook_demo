5f49d501a528026cffa22c813b50f40bc223235a
Mosi <mosisu07@gmail.com>
2024-08-22 18:17:59 +0800

    chore: added table enum

---
+++ src/enums/IconButton/IconButtonTableTypeEnum.ts
@@ -0,0 +1,11 @@
+/**
+ * 按鈕圖示類型
+ * @enum {string}
+ * @example
+ * import {IconButtonIconType} from './enums/IconButton/IconButtonIconType'
+ * console.log(IconButtonIconType.A) // 'a'
+ */
+export enum IconButtonTableTypeEnum {
+  A = "a",
+  B = "b",
+}
