1554cfbfa668869d08c31e3bcf4cd68339c21096
Mosi <mosisu07@gmail.com>
2024-08-27 09:51:12 +0800

    chore: added dependencies

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,84 +1,87 @@
 import {
-  Pagination as MuiPagination,
-  PaginationProps as MuiPaginationProps,
+    Pagination as MuiPagination,
+    PaginationProps as MuiPaginationProps,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 
 interface PaginationProps extends MuiPaginationProps {
-  totalPageSize: number; // 總頁數
-  totalSize: number; // 總筆數
-  onPageOnChange: (pageNum: number, pageSize: number) => void; // 點擊頁碼
-  // onPageSizeChange: (pageSize: number) => void; // 點擊每頁筆數
+    totalPageSize: number; // 總頁數
+    totalSize: number; // 總筆數
+    onPageOnChange: (pageNum: number, pageSize: number) => void; // 點擊頁碼
+    // onPageSizeChange: (pageSize: number) => void; // 點擊每頁筆數
 }
 
 const Pagination = ({
-  totalPageSize = 0, // 總頁數
-  totalSize = 0, // 總筆數
-  onPageOnChange = () => {}, // 點擊頁碼
-  ...props
+    totalPageSize = 0, // 總頁數
+    totalSize = 0, // 總筆數
+    onPageOnChange = () => {}, // 點擊頁碼
+    ...props
 }: PaginationProps) => {
-  // 目前頁數
-  const [currentPage, setCurrentPage] = useState(1);
-  // 每頁筆數
-  const [pageSize, setPageSize] = useState(10);
-  // 每頁筆數可選項
-  const defaultPageSizeArray = [10, 20, 30, 40, 50];
+    // 目前頁數
+    const [currentPage, setCurrentPage] = useState(1);
+    // 每頁筆數
+    const [pageSize, setPageSize] = useState(10);
+    // 每頁筆數可選項
+    const defaultPageSizeArray = [10, 20, 30, 40, 50];
 
-  const handlePageSizeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newPageSize = parseInt(e.target.value);
-    setPageSize(newPageSize);
-    setCurrentPage(1);
-  };
+    const handlePageSizeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newPageSize = parseInt(e.target.value);
+        setPageSize(newPageSize);
+        setCurrentPage(1);
+    };
 
-  useEffect(() => {
-    onPageOnChange(currentPage, pageSize);
-  }, [currentPage, pageSize]);
+    useEffect(() => {
+        onPageOnChange(currentPage, pageSize);
+    }, [currentPage, onPageOnChange, pageSize]);
 
-  // 外層樣式
-  const outerStyle = {
-    display: "flex",
-    alignItems: "center",
-    gap: 16,
-  };
-  // 內層樣式
-  const innerStyle = {
-    display: "flex",
-    alignItems: "center",
-    gap: 8,
-  };
+    // 外層樣式
+    const outerStyle = {
+        display: "flex",
+        alignItems: "center",
+        gap: 16,
+    };
+    // 內層樣式
+    const innerStyle = {
+        display: "flex",
+        alignItems: "center",
+        gap: 8,
+    };
 
-  return (
-    <div style={outerStyle}>
-      <MuiPagination
-        variant="outlined"
-        color="primary"
-        shape="rounded"
-        count={totalPageSize}
-        boundaryCount={4}
-        siblingCount={0}
-        defaultPage={1}
-        {...props}
-        onChange={(_, page) => setCurrentPage(page)}
-      />
-      <div style={outerStyle}>
-        <p>共 {totalPageSize} 頁</p>
-        <p style={innerStyle}>
-          每頁
-          {
-            <select value={pageSize} onChange={handlePageSizeChange}>
-              {defaultPageSizeArray.map((item) => (
-                <option key={item} value={item}>
-                  {item}
-                </option>
-              ))}
-            </select>
-          }
-          筆
-        </p>
-        <p>總筆數：{totalSize}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div style={outerStyle}>
+            <MuiPagination
+                variant="outlined"
+                color="primary"
+                shape="rounded"
+                count={totalPageSize}
+                boundaryCount={4}
+                siblingCount={0}
+                defaultPage={1}
+                {...props}
+                onChange={(_, page) => setCurrentPage(page)}
+            />
+            <div style={outerStyle}>
+                <p>共 {totalPageSize} 頁</p>
+                <p style={innerStyle}>
+                    每頁
+                    {
+                        <select
+                            value={pageSize}
+                            onChange={handlePageSizeChange}
+                        >
+                            {defaultPageSizeArray.map((item) => (
+                                <option key={item} value={item}>
+                                    {item}
+                                </option>
+                            ))}
+                        </select>
+                    }
+                    筆
+                </p>
+                <p>總筆數：{totalSize}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Pagination;
