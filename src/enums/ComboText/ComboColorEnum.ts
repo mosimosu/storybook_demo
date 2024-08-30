d250d04ca7caee7d7717560ba719f931b61acbfa
Mosi <mosisu07@gmail.com>
2024-08-30 17:45:29 +0800

    chore: created color enum

---
+++ src/enums/ComboText/ComboColorEnum.ts
@@ -0,0 +1,11 @@
+export enum ComboColorEnum {
+    Reserved = "#F9C97C",
+    Scheduled = "#90CDF4",
+    Break = "#D4D4D8",
+    Closed = "#D4D4D8",
+    Full = "#FC8181",
+
+    Morning = "#BEE3F8",
+    Afternoon = "#FDEDD3",
+    Evening = "#E9D8FD",
+}
