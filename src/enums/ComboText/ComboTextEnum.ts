c4045dbb8ace797fea541a680dc82d02e3eed2f1
Mosi <mosisu07@gmail.com>
2024-08-30 17:45:50 +0800

    chore: created text enum

---
+++ src/enums/ComboText/ComboTextEnum.ts
@@ -0,0 +1,11 @@
+export enum ComboTextEnum {
+    Reserved = "保留",
+    Scheduled = "預排",
+    Break = "休息",
+    Closed = "關閉",
+    Full = "滿",
+
+    Morning = "早診",
+    Afternoon = "午診",
+    Evening = "晚診",
+}
