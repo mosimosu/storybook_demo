7a1a42c50ede4ed4b3865d380b5e75944491d9d0
Mosi <mosisu07@gmail.com>
2024-09-05 17:31:10 +0800

    chore: deleted useless attributes

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -5,72 +5,61 @@ import { ButtonColorEnum } from "../enums/Button/ButtonColorEnum";
 import { ButtonVariantEnum } from "../enums/Button/ButtonVariantEnum";
 
 const meta: Meta<typeof Button> = {
-  // 設定元件名稱及位置
-  title: "Component/Button",
-  // 引入元件
-  component: Button,
-  // 把元件置中
-  parameters: {
-    layout: "centered",
-  },
-  // 自動生成 index 文件
-  tags: ["autodocs"],
-  // 設定元件控制項
-  argTypes: {
-    // 按鈕文字
-    children: {
-      control: "text",
-      description: "按鈕文字",
+    // 設定元件名稱及位置
+    title: "Component/Button",
+    // 引入元件
+    component: Button,
+    // 把元件置中
+    parameters: {
+        layout: "centered",
     },
-    // 按鈕種類
-    variant: {
-      control: "radio",
-      options: Object.values(ButtonVariantEnum).filter(
-        (variant) => variant !== ButtonVariantEnum.Text
-      ),
-      description: "按鈕種類",
-      defaultValue: "contained",
-    },
-    // 按鈕顏色
-    color: {
-      control: "select",
-      options: Object.values(ButtonColorEnum),
-      description: "按鈕顏色",
-      defaultValue: "primary",
-    },
-    // 是否禁用
-    disabled: {
-      control: "boolean",
-      description: "是否禁用",
-    },
-    // 是否選中
-    isSelected: {
-      control: "boolean",
-      description:
-        "是否選中，僅限 variant 為 outlined 且 color 為 primary 時有效",
-      if: {
-        arg: "variant",
-        eq: "outlined",
-      },
-    },
+    // 自動生成 index 文件
+    tags: ["autodocs"],
+    // 設定元件控制項
+    argTypes: {
+        // 按鈕文字
+        children: {
+            control: "text",
+            description: "按鈕文字",
+        },
+        // 按鈕種類
+        variant: {
+            control: "radio",
+            options: Object.values(ButtonVariantEnum),
+            description: "按鈕種類",
+            defaultValue: "contained",
+        },
+        // 按鈕顏色
+        color: {
+            control: "select",
+            options: Object.values(ButtonColorEnum).filter(
+                (item) => item !== "warning" && item !== "error"
+            ),
+            description: "按鈕顏色",
+            defaultValue: "primary",
+        },
+        // 是否禁用
+        disabled: {
+            control: "boolean",
+            description: "是否禁用",
+        },
 
-    // 按鈕大小
-    size: {
-      control: "select",
-      options: Object.values(ButtonSizeEnum),
-      description: "按鈕大小",
-      defaultValue: "medium",
+        // 按鈕大小
+        size: {
+            control: "select",
+            options: Object.values(ButtonSizeEnum),
+            description: "按鈕大小",
+            defaultValue: "medium",
+        },
     },
-  },
-  // 設定元件預設值
-  args: {
-    children: "按鈕",
-    variant: "contained",
-    color: "primary",
-    disabled: false,
-    isSelected: false,
-    size: "medium",
-  },
+    // 設定元件預設值
+    args: {
+        children: "按鈕",
+        variant: "contained",
+        color: "primary",
+        disabled: false,
+        size: "medium",
+    },
 };
 // 匯出元件
 export default meta;
@@ -79,46 +68,38 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
-  args: {
-    color: "primary",
-    disabled: false,
-  },
+    args: {
+        color: "primary",
+        disabled: false,
+    },
 };
 
 export const Secondary: Story = {
-  args: {
-    color: "secondary",
-    disabled: false,
-  },
+    args: {
+        color: "secondary",
+        disabled: false,
+    },
 };
 
-export const Warning: Story = {
-  args: {
-    color: "warning",
-    disabled: false,
-  },
-};
-
-export const Alarm: Story = {
-  args: {
-    color: "error",
-    disabled: false,
-  },
+export const Success: Story = {
+    args: {
+        color: "success",
+        disabled: false,
+    },
 };
 
 export const OutlinePrimary: Story = {
-  args: {
-    color: "primary",
-    variant: "outlined",
-    disabled: false,
-    isSelected: false,
-  },
+    args: {
+        color: "primary",
+        variant: "outlined",
+        disabled: false,
+    },
 };
 
 export const OutlineSecondary: Story = {
-  args: {
-    color: "secondary",
-    variant: "outlined",
-    disabled: false,
-  },
+    args: {
+        color: "secondary",
+        variant: "outlined",
+        disabled: false,
+    },
 };
