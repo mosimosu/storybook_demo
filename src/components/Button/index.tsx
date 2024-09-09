5812310d06b6dd26fbe992d700fae7777099aad8
Mosi <mosisu07@gmail.com>
2024-09-09 18:17:09 +0800

    refactor: added styled version of button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,8 @@
 import {
     Button as MuiButton,
     ButtonProps as MuiButtonProps,
-} from "@mui/material";
+    styled
+} from '@mui/material'
 
 /**
  * Button 元件屬性 extends MUI Button 屬性
@@ -10,8 +11,33 @@ import {
  * @extends {MuiButtonProps}
  */
 interface ButtonProps extends MuiButtonProps {
-    text: string;
+    text: string
 }
+
+const StyledButton = styled(MuiButton)(({ theme }) => ({
+    '.MuiButton-containedPrimary': {
+        '&.Mui-disabled': {
+            color: theme.palette.common.white
+        }
+    },
+    '.MuiButton-containedSecondary': {
+        '&.Mui-disabled': {
+            color: theme.palette.common.white
+        }
+    },
+    '.MuiButton-outlinedPrimary': {
+        borderColor: theme.palette.primary.main,
+        boxShadow:
+            '0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)',
+        '&:hover': {
+            borderColor: theme.palette.primary.light,
+            boxShadow: 'none'
+        },
+        '&.Mui-disabled': {
+            color: theme.palette.grey[500]
+        }
+    }
+}))
 
 /**
  * Button 元件
@@ -24,7 +50,7 @@ interface ButtonProps extends MuiButtonProps {
  */
 const Button = ({
     // isSelected = false,
-    text = "按鈕",
+    text = '按鈕',
     ...props
 }: ButtonProps): JSX.Element => {
     // // 選取時的樣式
@@ -32,18 +58,18 @@ const Button = ({
     // 組合 MUI Button 的 sx 屬性和外部傳入的 sx 屬性
     const totalStyle = {
         // ...selectedStyle,
-        ...props.sx,
-    };
+        ...props.sx
+    }
 
     return (
-        <MuiButton
+        <StyledButton
             variant="contained" // 按鈕樣式，預設為 contained，其他可選擇 text、outlined
             color="primary" // 按鈕顏色，預設為 primary，其他可選擇 success、error、warning、info、secondary、info
             {...props}
             sx={totalStyle}
         >
             {text}
-        </MuiButton>
-    );
-};
-export default Button;
+        </StyledButton>
+    )
+}
+export default Button
