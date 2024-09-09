17700f0857b0ac9099c8835c2bdd769ab5e3a7b5
Mosi <mosisu07@gmail.com>
2024-09-09 22:03:16 +0800

    style: added 3 variants of button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,10 @@
+import { BorderColor } from "@mui/icons-material";
 import {
-    Button as MuiButton,
-    ButtonProps as MuiButtonProps,
-    styled,
-    alpha
-} from '@mui/material'
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+  styled,
+  alpha,
+} from "@mui/material";
 
 /**
  * Button 元件屬性 extends MUI Button 屬性
@@ -12,61 +13,87 @@ import {
  * @extends {MuiButtonProps}
  */
 interface ButtonProps extends MuiButtonProps {
-    text: string
+  text: string;
 }
 
 const StyledButton = styled(MuiButton)(({ theme }) => ({
-    '.MuiButton-containedPrimary': {
-        '&.Mui-disabled': {
-            color: theme.palette.common.white
-        }
+  ".MuiButton-containedPrimary": {
+    "&.Mui-disabled": {
+      color: theme.palette.common.white,
     },
-    '.MuiButton-containedSecondary': {
-        '&.Mui-disabled': {
-            color: theme.palette.common.white
-        }
+  },
+  ".MuiButton-containedSecondary": {
+    "&.Mui-disabled": {
+      color: theme.palette.common.white,
     },
-    '.MuiButton-outlinedPrimary': {
-        borderColor: theme.palette.primary.main,
-        boxShadow:
-            '0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)',
-        '&:hover': {
-            borderColor: theme.palette.primary.light,
-            boxShadow: 'none'
-        },
-        '&.Mui-disabled': {
-            color: theme.palette.grey[500]
-        }
+  },
+  ".MuiButton-outlinedPrimary": {
+    borderColor: theme.palette.primary.main,
+    boxShadow:
+      "0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)",
+    "&:hover": {
+      borderColor: theme.palette.primary.light,
+      boxShadow: "none",
     },
-    '.MuiButton-outlinedSecondary': {
-        color: theme.palette.common.black,
-        borderColor:theme.palette.grey[600],
-        boxShadow:'0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)',
-        '&:hover': {
-            borderColor: theme.palette.grey[600],
-            backgroundColor: alpha(theme.palette.common.black, 0.05),
-            boxShadow: 'none'
-        },
-        '&.Mui-disabled': {
-            borderColor: alpha('#111111', 0.15),
-            color: theme.palette.grey[500]
-        }
+    "&.Mui-disabled": {
+      color: theme.palette.grey[500],
     },
-    '.MuiButton-outlineSuccess':{
-        borderColor: theme.palette.success.main,
-        boxShadow:
-            '0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)',
-        '&:hover': {
-            borderColor: theme.palette.success.main,
-            backgroundColor: alpha(theme.palette.success.main, 0.05),
-            boxShadow: 'none'
-        },
-        '&.Mui-disabled': {
-            borderColor: alpha('#111111', 0.15),
-            color: theme.palette.grey[500]
-        }
-    }
-}))
+  },
+  ".MuiButton-outlinedSecondary": {
+    color: theme.palette.common.black,
+    borderColor: theme.palette.grey[600],
+    boxShadow:
+      "0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)",
+    "&:hover": {
+      borderColor: theme.palette.grey[600],
+      backgroundColor: alpha(theme.palette.common.black, 0.05),
+      boxShadow: "none",
+    },
+    "&.Mui-disabled": {
+      borderColor: alpha("#111111", 0.15),
+      color: theme.palette.grey[500],
+    },
+  },
+  ".MuiButton-outlineSuccess": {
+    borderColor: theme.palette.success.main,
+    boxShadow:
+      "0 2px 2px 0 rgba(0, 0, 0, 0.15), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.10)",
+    "&:hover": {
+      borderColor: theme.palette.success.main,
+      backgroundColor: alpha(theme.palette.success.main, 0.05),
+      boxShadow: "none",
+    },
+    "&.Mui-disabled": {
+      borderColor: alpha("#111111", 0.15),
+      color: theme.palette.grey[500],
+    },
+  },
+  ".MuiButton-textPrimary": {
+    color: theme.palette.primary.main,
+    "&.Mui-disabled": {
+      color: theme.palette.grey[500],
+    },
+  },
+  ".MuiButton-textSecondary": {
+    color: theme.palette.common.black,
+    "&:hover": {
+      BorderColor: theme.palette.grey[600],
+      backgroundColor: alpha("#000000", 0.05),
+    },
+    "&.Mui-disabled": {
+      BorderColor: alpha("#111111", 0.15),
+      color: theme.palette.grey[500],
+    },
+  },
+  ".MuiButton-textSuccess": {
+    color: theme.palette.success.main,
+    backgroundColor: alpha(theme.palette.success.main, 0.05),
+    "&.Mui-disabled": {
+      BorderColor: alpha("#111111", 0.15),
+      color: theme.palette.grey[500],
+    },
+  },
+}));
 
 /**
  * Button 元件
@@ -78,27 +105,27 @@ const StyledButton = styled(MuiButton)(({ theme }) => ({
  * @return {JSX.Element}
  */
 const Button = ({
-    // isSelected = false,
-    text = '按鈕',
-    ...props
+  // isSelected = false,
+  text = "按鈕",
+  ...props
 }: ButtonProps): JSX.Element => {
-    // // 選取時的樣式
-    // const selectedStyle = isSelected && { backgroundColor: Palette.Blue50 };
-    // 組合 MUI Button 的 sx 屬性和外部傳入的 sx 屬性
-    const totalStyle = {
-        // ...selectedStyle,
-        ...props.sx
-    }
+  // // 選取時的樣式
+  // const selectedStyle = isSelected && { backgroundColor: Palette.Blue50 };
+  // 組合 MUI Button 的 sx 屬性和外部傳入的 sx 屬性
+  const totalStyle = {
+    // ...selectedStyle,
+    ...props.sx,
+  };
 
-    return (
-        <StyledButton
-            variant="contained" // 按鈕樣式，預設為 contained，其他可選擇 text、outlined
-            color="primary" // 按鈕顏色，預設為 primary，其他可選擇 success、error、warning、info、secondary、info
-            {...props}
-            sx={totalStyle}
-        >
-            {text}
-        </StyledButton>
-    )
-}
-export default Button
+  return (
+    <StyledButton
+      variant="contained" // 按鈕樣式，預設為 contained，其他可選擇 text、outlined
+      color="primary" // 按鈕顏色，預設為 primary，其他可選擇 success、error、warning、info、secondary、info
+      {...props}
+      sx={totalStyle}
+    >
+      {text}
+    </StyledButton>
+  );
+};
+export default Button;
