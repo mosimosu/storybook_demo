32eff34310f3d03a46231d40987bd8d303e2b106
Mosi <mosisu07@gmail.com>
2024-09-17 23:31:39 +0800

    chore: created iconEnum & imported to button component

---
+++ src/components/Icons/IconEnum.ts
@@ -0,0 +1,13 @@
+export enum IconEnum {
+    Add = 'Add',
+    Edit = 'Edit',
+    View = 'View',
+    Export = 'Export',
+    Print = 'Print',
+    Copy = 'Copy',
+    Cancel = 'Cancel',
+    Save = 'Save',
+    Delete = 'Delete',
+    Reply = 'Reply',
+    Search = 'Search',
+}
